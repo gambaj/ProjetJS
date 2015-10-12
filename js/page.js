84b2c921fec81bcab4874faaa36627412ef5a601
gambaj <jordann.gamba@gmail.com>
2015-10-12 11:24:09 +0200

    Transformation des fonctions isolées en classe/prototype

--- js/page.js
+++ js/page.js
@@ -1,36 +1,80 @@
-$('.datetime').datetimepicker({
-    dateFormat: "dd/mm/yy",
-    timeFormat:  "HH:mm",
-    language:  'fr',
-    weekStart: 1,
-    todayBtn:  1,
-    autoclose: 1,
-    todayHighlight: 1,
-    startView: 2,
-    forceParse: 0,
-    showMeridian: 1
-});
-
-function height(bloc){
-
-    var hauteur;
-    if(typeof( window.innerWidth )=='number')
-        hauteur = window.innerHeight;
-
-    else if( document.documentElement && document.documentElement.clientHeight )
-        hauteur = document.documentElement.clientHeight;
-
-    document.getElementById(bloc).style.height = hauteur+"px";
+// Définition de l'objet Page qui va initialiser la page et le formulaire.
+function Page() {
+    this.hauteur = 0;
 }
 
-window.onload = function(){ height("divTableau"); height("divMenu") }
-$("#formulaire").hide();
-$("#Enregistrer").hide();
+// Ce qui change pas est ici, ce qui change est là haut.
+Page.prototype = {
 
-function afficheFormulaire() {
-    $('#textInputSession').prop('disabled', true);
-    $('#formulaire').show();
-    $('#Enregistrer').show();
+    /**
+     * Cette fonction initialise la taille de la page et le formulaire
+     */
+    initialiser:function() {
+
+        var page = this;
+        window.onload = function(){
+            page.fixerTaille();
+        }
+        page.afficheFormulaire();
+        page.timedate();
+    },
+
+    /**
+     * Cette fonction calcule la taille verticale de l'ecran en pixel et la fixe à un bloc.
+     * @param bloc le bloc dont on souhaite modifier la taille.
+     */
+    height:function height(bloc){
+
+        if(typeof( window.innerWidth )=='number')
+            this.hauteur = window.innerHeight;
+
+        else if( document.documentElement && document.documentElement.clientHeight )
+            this.hauteur = document.documentElement.clientHeight;
+
+        document.getElementById(bloc).style.height = this.hauteur+"px";
+    },
+
+    /**
+     * Cette fonction fixe la taille des deux blocs principaux à la taille de l'ecran.
+     */
+    fixerTaille:function () {
+
+        this.height("divTableau");
+        this.height("divMenu");
+    },
+
+    /**
+     * Cette fonction cache le formulaire.
+     */
+    afficheFormulaire:function() {
+
+        $("#formulaire").hide();
+        $("#impression").hide();
+        $("#button").click(function() {
+            $('#textInputSession').prop('disabled', true);
+            $('#formulaire').show();
+            $('#impression').show();
+        });
+    },
+
+    /**
+     * Initialiser le format du champ "date".
+     */
+    timedate:function() {
+
+        $('.datetime').datetimepicker({
+            dateFormat: "dd/mm/yy",
+            timeFormat:  "HH:mm",
+            language:  'fr',
+            weekStart: 1,
+            todayBtn:  1,
+            autoclose: 1,
+            todayHighlight: 1,
+            startView: 2,
+            forceParse: 0,
+            showMeridian: 1
+        });
+    }
 }
 
 function generateImages() {
