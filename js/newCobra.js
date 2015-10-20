25c97dc24939e854f7b767fd7d7db5b004d27e8e
TsongTsong <xulyc9a@gmail.com>
2015-10-20 16:17:10 +0200

    amélioration_1 pour  Cobra

--- js/newCobra.js
+++ js/newCobra.js
@@ -1,32 +1,50 @@
 
         var cobra = new Cobra();
-        var room = "chat";
+        var room = "weCreateNote";
         var url = null;
         var socketId = null;
+	    var apiUrl = 'http://cobra-framework.com:3000/api/events/' + room;
 
-        cobra.connect("http://cobra-framework.com:8080");
 
         cobra.connectionCallback = function(){
             cobra.joinRoom(room);
         };
 
-        cobra.joinRoomCallback = function(){
-            //cobra.sendMessage({type:"join", login:login},room);
-        };
+        cobra.joinRoomCallback = function (roomName) {
+           // appel à l'API pour récupérer tous les messages de la room roomName
+           $.ajax({
+              type: 'GET',
+              url: apiUrl,
+              success: function () {
+                  console.log("success");
+              },
+
+              error: function () {
+                  console.log("error when retrieve events");
+              },
+
+              complete: function (result, status) {
+                  console.log("complete");
+
+                  for (var i = 0; i < result.responseJSON.Events.length; i++) {
+                     var content = JSON.parse(result.responseJSON.Events[i].content);
+                      creationNote(content.message.title, content.message.content, content.message.user, content.message.date);
+                  }
+
+                  // Pour envoyer un message dans toute la room
+                  //cobra.sendMessage({content : "test"}, room, true);
+
+                  // Pour envoyer un message dans toute la room excepté soi
+                  // cobra.sendMessage({content : "test"}, room, false);
+              }
+          });
+        }
 
         cobra.messageReceivedCallback = function(message){
-            console.log("message received callback");
             if(message.type == "infos"){
-                console.log("infos");
-                for(var i = 0; i < message.clients.length; i++)
-                {
-                    var client = message.clients[i];
-                }
                 socketId = message.socketId;
-                console.log("infos1111");
             }
             else{
-                console.log("else");
                 if (socketId == message.socketId) {
                     creationNote(message.message.title, message.message.content, message.message.user, message.message.date);
                 }
@@ -52,10 +70,20 @@
                     cobra.sendMessage(message, room, true);
                     $("#textInputTitle").val("");
                     $("#textInputText").val("");
-                    $("#textInputSession").val("");
+                    //$("#textInputSession").val("");
                     $("#textInputDate").val("");
                     $("#textInputTitle").focus();
                 }
             }
+
+            $("#buttonConnexion").click(function(){
+                var user=$("#textInputSession").val();
+                if(user){
+                    cobra.connect("http://cobra-framework.com:8080");
+                }
+                else{
+                    alert("Veuillez saisir votre nom pour connecter!");
+                }
+            }) ;
         });
 
